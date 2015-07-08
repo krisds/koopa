e1894e692ba9e20a29eb8f88d8dc1be65d3083db
kdeschut <kdeschut@07b9d407-957a-469a-9197-ae794bbef2a3>
2015-07-08 07:36:46 +0000

    Working on copybook resolution.
    Added some extra logging.
    Made it smarter about matching literal names.
    
    
    
    
    git-svn-id: svn://svn.code.sf.net/p/koopa/code/trunk@279 07b9d407-957a-469a-9197-ae794bbef2a3

--- src/cobol/koopa/cobol/Copybooks.java
+++ src/cobol/koopa/cobol/Copybooks.java
@@ -6,10 +6,14 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import org.apache.log4j.Logger;
+
 import koopa.core.util.Files;
 
 /** EXPERIMENTAL ! Support for pre-processing. */
 public class Copybooks {
+	private static final Logger LOGGER = Logger.getLogger("copybooks");
+
 	private List<File> copybookPaths = new ArrayList<File>();
 
 	public void setLookupPaths(List<File> copybookPaths) {
@@ -17,22 +21,64 @@ public class Copybooks {
 	}
 
 	// TODO Take library name into account.
-	public File lookup(final String textName, final String libraryName) {
-		if (copybookPaths == null)
+	public File lookup(String textName, final String libraryName) {
+		// Were we given a literal name ?
+		final boolean isLiteralTextName = (textName.startsWith("\"") && textName
+				.endsWith("\""))
+				|| (textName.startsWith("'") && textName.endsWith("'"));
+
+		// So what are we looking for ?
+		final String fileName;
+		if (isLiteralTextName)
+			fileName = textName.substring(1, textName.length() - 1);
+		else
+			fileName = textName;
+
+		if (copybookPaths == null) {
+			if (LOGGER.isInfoEnabled())
+				LOGGER.info("Lookup of copybook " + textName + " in "
+						+ libraryName + " failed: no copybook paths were set.");
 			return null;
+		}
 
 		for (File path : copybookPaths) {
 			File[] matches = path.listFiles(new FilenameFilter() {
 				public boolean accept(File path, String name) {
-					return CobolFiles.isCopybook(name)
-							&& Files.getName(name).equalsIgnoreCase(textName);
+					if (isLiteralTextName && fileName.equalsIgnoreCase(name)) {
+						if (LOGGER.isTraceEnabled())
+							LOGGER.info("Lookup of copybook " + fileName
+									+ " in " + libraryName + "; testing "
+									+ name + "; names match exactly.");
+
+						return true;
+					}
+
+					final boolean isCopybook = CobolFiles.isCopybook(name);
+					final boolean namesMatch = Files.getName(name)
+							.equalsIgnoreCase(Files.getName(fileName));
+
+					if (LOGGER.isTraceEnabled())
+						LOGGER.info("Lookup of copybook " + fileName + " in "
+								+ libraryName + "; testing " + name
+								+ "; copybook: " + isCopybook
+								+ "; names match: " + namesMatch);
+
+					return isCopybook && namesMatch;
 				}
 			});
 
-			if (matches != null && matches.length > 0)
+			if (matches != null && matches.length > 0) {
+				if (LOGGER.isInfoEnabled())
+					LOGGER.info("Lookup of copybook " + textName + " in "
+							+ libraryName + " successfull; found " + matches[0]);
+
 				return matches[0];
+			}
 		}
 
+		if (LOGGER.isInfoEnabled())
+			LOGGER.info("Lookup of copybook " + textName + " in " + libraryName
+					+ " failed: not found in " + copybookPaths);
 		return null;
 	}
 
